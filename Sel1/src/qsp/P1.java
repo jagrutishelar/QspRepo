5341c786b3f43d4f08b4738dffd14ee9d65a8fdf
admin <admin@DESKTOP-R4959BB>
2021-04-03 16:27:14 +0530

    dummy project

---
+++ Sel1/src/qsp/P1.java
@@ -0,0 +1,26 @@
+package qsp;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class P1 {
+	
+	static {
+		System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
+	}
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://demo.actitime.com/login.do");
+		driver.findElement(By.name("username")).sendKeys("admin");
+		Thread.sleep(3000);
+		driver.findElement(By.name("pwd")).sendKeys("manager");
+		Thread.sleep(3000);
+		driver.findElement(By.xpath("//div[.='Login ']")).click();
+		//driver.close();
+
+	}
+
+}
