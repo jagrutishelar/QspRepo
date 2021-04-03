3eca4a20a17ab6b63540f131587e53972add214a
admin <admin@DESKTOP-R4959BB>
2021-04-03 16:32:26 +0530

    uploading updated project

--- Sel1/src/qsp/P1.java
+++ Sel1/src/qsp/P1.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class P1 {
+public class P1 {//P1
 	
 	static {
 		System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
---
+++ Sel1/src/qsp/P2.java
@@ -0,0 +1,10 @@
+package qsp;
+
+public class P2 {
+
+	public static void main(String[] args) {
+		System.out.println("from P2");
+
+	}
+
+}
